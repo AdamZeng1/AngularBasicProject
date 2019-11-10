02a4642b5fe66ad26043eca561cb9c852dda60d4
adamzeng <khklara3@gmail.com>
2019-11-10 16:28:19 +0800

    self-define-directive-toggle-menu

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,16 @@
+import {Directive, HostListener, HostBinding, ElementRef} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    // if this element contain event's target, it means click itself or child element. if not, click outside the element
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  }
+
+  constructor(private elRef: ElementRef) {
+  }
+}
